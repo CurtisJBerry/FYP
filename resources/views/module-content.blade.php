a792d28c2777d398419342527a8769fa59a42541
Curtis Berry <u1971628@unimail.hud.ac.uk>
2022-03-28 16:47:51 +0100

    Added buttons to create new subjects/modules with error handling to make sure there are no duplicates

--- resources/views/module-content.blade.php
+++ resources/views/module-content.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('View a Module') }}
+            {{ __('View a Module: '.$module->module_name) }}
         </h2>
     </x-slot>
 
--- resources/views/subjects.blade.php
+++ resources/views/subjects.blade.php
@@ -11,6 +11,11 @@
                 <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                         <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                            @if(Auth::user()->user_type !== "user")
+                                <button type="button" class="btn btn-success float-left" data-toggle="modal" data-target="#addSubjectModal">
+                                    Add Subject
+                                </button>
+                            @endif
                             <table class="w-full">
                                 <thead class="bg-gray-50">
                                 <tr>
@@ -35,7 +40,7 @@
                                                 <div class="text-sm text-gray-500">{{ $subject->exam_board }}</div>
                                             </td>
                                             <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ $subject->subject_level }}</td>
-                                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{ $subject->description }}</td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500" style="overflow-wrap: normal">{{ $subject->description }}</td>
                                         </tr>
                                     @endforeach
                                 @else
@@ -54,6 +59,48 @@
                         </div>
                     </div>
                     {{$subjects->links()}}
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Add Subject Modal -->
+    <div class="modal fade" id="addSubjectModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Add a new Subject</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('subjects.store') }}" method="POST">
+                        @csrf
+                        @method('POST')
+                        <div class="form-group">
+                            <label for="subjectname" class="col-form-label">Subject Name:</label>
+                            <input type="text" class="form-control" id="subjectname" name="subjectname" maxlength="30" placeholder="E.g English Language" required>
+                        </div>
+                        <div class="form-group">
+                            <div class="form-group">
+                                <label for="exam" class="col-form-label">Exam Board:</label>
+                                <input type="text" class="form-control" id="exam" name="exam" maxlength="20" placeholder="E.g AQA or CCEA" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="level" class="col-form-label">Subject Level:</label>
+                            <input type="text" class="form-control" id="level" name="level" maxlength="8" placeholder="E.g GCSE" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="description" class="col-form-label">Description:</label>
+                            <textarea class="form-control" id="description" name="description" placeholder="A short outline of what the subject involves" required></textarea>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-success">Add Subject</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
