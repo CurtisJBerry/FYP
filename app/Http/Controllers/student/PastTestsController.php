47bc6922dfd856c7bf82eb8a6652dc555e513be3
Curtis Berry <u1971628@unimail.hud.ac.uk>
2022-04-26 20:42:57 +0100

    Added page to show scores for each test with average and max to give a good overview

--- app/Http/Controllers/student/PastTestsController.php
+++ app/Http/Controllers/student/PastTestsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\student;
 
 use App\Http\Controllers\Controller;
+use App\Models\Test;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -10,18 +11,54 @@ use Illuminate\Support\Facades\Auth;
 class PastTestsController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display all the past tests
      *
      * @return \Illuminate\Http\Response
      */
     public function index() {
 
-        $user = User::where('id','=', Auth::user()->id)->whereHas('tests', function ($query){
+        $user = User::where('id','=', Auth::user()->id)->with('tests', function ($query){
             $query->groupBy('test_id');
         })->paginate(5);
 
 
         return view('student/past-tests', ['user' => $user]);
 
+    }
+
+    /**
+     * Show the test scores for the given test id
+     *
+     * @param $id
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function show($id) {
+
+        $test = Test::where('id', $id)->first();
+
+
+        $user = User::where('id','=', Auth::user()->id)->with('tests', function ($query) use ($id) {
+            $query->where('test_id', $id);
+        })->paginate(10);
+
+        $totalScore = 0;
+        $testcount = 0;
+        $highestScore = 0;
+
+        foreach($user as $item){
+            foreach($item->tests as $test){
+                ++$testcount;
+                $totalScore += $test->pivot->score;
+                if($test->pivot->score > $highestScore){
+                    $highestScore = $test->pivot->score;
+                }
+            }
+        }
+
+        $averageScore = $totalScore / $testcount;
+
+        $averageScore = round($averageScore, 2);
+
+        return view('student/past-test-scores', compact('user', 'test', 'averageScore', 'highestScore'));
     }
 }
--- resources/views/student/past-tests.blade.php
+++ resources/views/student/past-tests.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Past Test Scores') }}
+            {{ __('Past Tests') }}
         </h2>
     </x-slot>
 
@@ -11,18 +11,20 @@
                 <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                         <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-
+                            <div class="p-6 bg-white border-b border-gray-200">
+                                <a href="{{ route('home')}}"><button type="button" class="btn btn-primary float-right">
+                                        Go Back
+                                    </button></a>
+                                <h3 class="font-semibold text-xl text-gray-800 leading-tight">View Past Tests</h3>
+                            </div>
                             <div class="p-6 bg-white">
-
                                 <table class="w-full">
                                     <thead class="bg-gray-50">
                                     <tr>
                                         <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Test Name</th>
                                         <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">SubModule Name</th>
                                         <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Module Name</th>
-                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Score</th>
-                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Average Score Per Test</th>
-
+                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
                                     </tr>
                                     </thead>
                                     <tbody class="bg-white divide-y divide-gray-200">
@@ -34,7 +36,8 @@
                                                             <td class="px-6 py-4 whitespace-nowrap text-sm">{{$test->test_name}}</td>
                                                             <td class="px-6 py-4 whitespace-nowrap text-sm">{{$test->submodule->submodule_name}}</td>
                                                             <td class="px-6 py-4 whitespace-nowrap text-sm">{{$test->submodule->module->module_name}}</td>
-                                                            <td class="px-6 py-4 whitespace-nowrap text-sm">{{$test->pivot->score}}%</td>
+{{--                                                            <td class="px-6 py-4 whitespace-nowrap text-sm">{{$test->pivot->score}}%</td>--}}
+                                                            <td class="px-6 py-4 whitespace-nowrap text-sm"><a href="{{ route('past-tests.show', $test->id) }}"><button type="submit" class="btn btn-primary">View All Scores</button></a></td>
                                                         </tr>
                                                     @else
                                                         <tr>
@@ -47,7 +50,6 @@
                                                             </td>
                                                         </tr>
                                                     @endif
-
                                                 @endforeach
                                         @endforeach
                                     @endif
