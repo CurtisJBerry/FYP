eaa1ebb56c0bd285a315308dac85c9552c07642b
Curtis Berry <u1971628@unimail.hud.ac.uk>
2022-04-28 22:56:53 +0100

    Added error checking and return message if results cannot be found, condensed code into seperate functions

--- app/Http/Controllers/student/ProcessViewingDataController.php
+++ app/Http/Controllers/student/ProcessViewingDataController.php
@@ -28,22 +28,70 @@ class ProcessViewingDataController extends Controller
         $week2 = 30;
         $week1 = 40;
 
+        //get the data from the past 4 weeks
         $expDate = Carbon::now()->subDays(28);
-
-//        $tags = Tag::with(['resources.changelogs' => function($query) use ($expDate) {
-//            $query->where('created_at','>', $expDate)->with('resource.tags');
-//            }
-//        ])->get();
 
         $logs = Changelog::with('resource.tags')->where('user_id', $id)->where('created_at','>', $expDate)->get();
 
+        if (empty($logs)){
+            return back()->dangerBanner('You have now viewing data, use our service and look at some of the available resources to get a Learner Type suggestion.');
+        }else{
 
-        [$under21days, $over21days] = $logs->partition(function ($i) {
-            return $i['created_at'] < Carbon::now()->subDays(21);
-        });
+            [$under21days, $over21days] = $logs->partition(function ($i) {
+                return $i['created_at'] < Carbon::now()->subDays(21);
+            });
 
-        echo "Week 4: " . count($under21days) . "<br>";
-        $count4 = $this->getTagCount($under21days);
+            //get week 4 data
+            $count4 = $this->getTagCount($under21days);
+            $count4 = $this->getWeek4Data($week4, $count4);
+
+
+            [$under14days, $over14days] = $over21days->partition(function ($i){
+                return $i['created_at'] < Carbon::now()->subDays(14);
+            });
+
+
+            //get week 3 data
+            $count3 = $this->getTagCount($under14days);
+            $count3 = $this->getWeek3Data($week3, $count3, $count4);
+
+
+            [$under7days, $over7days] = $over14days->partition(function ($i){
+                return $i['created_at'] < Carbon::now()->subDays(7);
+            });
+
+            //get week 2 data
+            $count2 = $this->getTagCount($under7days);
+            $count2 = $this->getWeek2Data($week2, $count2, $count3);
+
+            //get week 1 data
+            $count1 = $this->getTagCount($over7days);
+            $count1 = $this->getWeek1Data($week1, $count1, $count2);
+
+
+            //get max value and key from array
+            $max = max($count1);
+            $key = array_search($max, $count1);
+
+            if (count(array_unique($count1)) == 2){
+                return back()->dangerBanner('Your viewing data is too similar to set your Learner Type.');
+            }else{
+
+                $user = User::where('id', $id)->first();
+
+                $user->learner_type = $key;
+                $user->save();
+
+                return back()->banner('Your Learner Type has been updated to ' . ucfirst($key) . ' as ' . $max  . '% of your viewing history matched this type.');
+            }
+
+        }
+
+
+    }
+
+    public function getWeek4Data($week4, $count4){
+
         $arraySum = array_sum($count4);
 
         if(!$arraySum == 0){
@@ -56,14 +104,11 @@ class ProcessViewingDataController extends Controller
         }
         unset($val, $arraySum);
 
+        return $count4;
+    }
 
-        [$under14days, $over14days] = $over21days->partition(function ($i){
-            return $i['created_at'] < Carbon::now()->subDays(14);
-        });
+    public function getWeek3Data($week3, $count3, $count4){
 
-        echo "Week 3: " . count($under14days) . "<br>";
-
-        $count3 = $this->getTagCount($under14days);
         $arraySum = array_sum($count3);
 
         if(!$arraySum == 0){
@@ -82,13 +127,12 @@ class ProcessViewingDataController extends Controller
         }
         unset($value, $val);
 
-        [$under7days, $over7days] = $over14days->partition(function ($i){
-            return $i['created_at'] < Carbon::now()->subDays(7);
-        });
+        return $count3;
 
-        echo "Week 2: " . count($under7days)  . "<br>";
+    }
 
-        $count2 = $this->getTagCount($under7days);
+    public function getWeek2Data($week2, $count2, $count3){
+
         $arraySum = array_sum($count2);
 
         if(!$arraySum == 0){
@@ -106,10 +150,12 @@ class ProcessViewingDataController extends Controller
         }
         unset($value, $val);
 
+        return $count2;
 
-        echo "Week 1: " . count($over7days) . "<br>";
+    }
 
-        $count1 = $this->getTagCount($over7days);
+    public function getWeek1Data($week1, $count1, $count2){
+
         $arraySum = array_sum($count1);
 
         if(!$arraySum == 0){
@@ -129,15 +175,8 @@ class ProcessViewingDataController extends Controller
         }
         unset($value, $val);
 
+        return $count1;
 
-        $max = max($count1);
-
-        $key = array_search($max, $count1);
-
-        echo "The max value is: " . $max . ", its key is " . $key;
-
-
-        //return back()->banner('Accessed page!');
     }
 
 
